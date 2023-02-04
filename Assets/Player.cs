3a64fc0b7d0d83a784b40ba3503d7ef62cf96d0a
Georgs Toliasvili <georgs.toliasvili@gmail.com>
2023-02-04 18:35:44 +0200

    Jumping in progress

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
+using Unity.Mathematics;
 
 public class Player : MonoBehaviour
 {
@@ -14,15 +15,19 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpHeight = 10f;
     [SerializeField] private float gravityScale = 5f;
 
+    private Matrix4x4 containersWorldMatrix;
+
     private SplinePath path;
     private float distanceTravelledAlongPath;
     private float yVelocity;
     private MovementDirection travelDirection;
 
+    private bool isGrounded = true;
+
     // Start is called before the first frame update
     private void Start()
     {
-        Matrix4x4 containersWorldMatrix = splineContainer.transform.localToWorldMatrix;
+        containersWorldMatrix = splineContainer.transform.localToWorldMatrix;
 
         path = CreateSplinePath(splineContainer, containersWorldMatrix);
     }
@@ -34,13 +39,32 @@ public class Player : MonoBehaviour
 
         HandleInput();
 
-        MoveAndRotate();
+        if (isGrounded)
+        {
+            MoveAndRotate();
+        }
+        else
+        {
+            NativeSpline native = new NativeSpline(path, containersWorldMatrix);
+            float distance = SplineUtility.GetNearestPoint(native, transform.position, out float3 position, out float t);
+            Debug.Log(distance);
+            if (distance < 0.5f)
+            {
+                distanceTravelledAlongPath = t;
+                transform.position = GetPathPosition();
+                isGrounded = true;
+                return;
+            }
 
-        //transform.Translate(new Vector3(0f, yVelocity, 0f) * Time.deltaTime);
+            transform.Translate(new Vector3(0f, yVelocity, 0f) * Time.deltaTime);
+        }
     }
 
     private void HandleInput()
     {
+        if (!isGrounded)
+            return;
+
         travelDirection = MovementDirection.NOT_MOVING;
         charAnimator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
 
@@ -63,6 +87,8 @@ public class Player : MonoBehaviour
     private void Jump()
     {
         yVelocity = Mathf.Sqrt(jumpHeight * -2 * (Physics.gravity.y * gravityScale));
+        transform.Translate(new Vector3(0f, yVelocity, 0f) * Time.deltaTime);
+        isGrounded = false;
     }
 
     private void MoveAndRotate()
@@ -74,6 +100,7 @@ public class Player : MonoBehaviour
 
         distanceToTravel *= (int)travelDirection;
         distanceTravelledAlongPath += distanceToTravel;
+        distanceTravelledAlongPath = Mathf.Clamp01(distanceTravelledAlongPath);
 
         Vector3 newPosition = GetPathPosition() + offsetFromPath;
         Vector3 newRotation = newPosition + (GetPathRotation() * (int)travelDirection);
